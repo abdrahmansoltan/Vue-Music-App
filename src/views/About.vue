5744ffa57984118dde7321b3375ed62d075eabe4
Abdelrahman Soltan <asoltan@instabug.com>
2023-01-25 08:57:12 +0200

    feat: :art: create main views pages

---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>ABout Page</template>
+
+<script>
+export default {
+  name: "About",
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- Introduction -->
+  <section class="mb-8 py-20 text-white text-center relative">
+    <div
+      class="absolute inset-0 w-full h-full bg-contain introduction-bg"
+      style="background-image: url(assets/img/header.png)"
+    ></div>
+    <div class="container mx-auto">
+      <div class="text-white main-header-content">
+        <h1 class="font-bold text-5xl mb-5">Listen to Great Music!</h1>
+        <p class="w-full md:w-8/12 mx-auto">
+          Listen to music uploaded by users, Upload your favorite music now and
+          share them with others!
+        </p>
+      </div>
+    </div>
+
+    <img
+      class="relative block mx-auto mt-5 -mb-20 w-auto max-w-full"
+      src="/assets/img/introduction-music.png"
+    />
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
