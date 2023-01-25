7aa0d6c9714fc0f317236696191249aecd0be5c0
Abdelrahman Soltan <asoltan@instabug.com>
2023-01-25 16:57:27 +0200

    feat: :sparkles: refactor Manage component with Upload and CompositionItem components

---
+++ src/components/Upload.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="bg-white rounded border border-gray-200 relative flex flex-col">
+    <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
+      <span class="card-title">Upload</span>
+      <i class="fas fa-upload float-right text-green-400 text-2xl"></i>
+    </div>
+    <div class="p-6">
+      <!-- Upload Dropbox -->
+      <div
+        class="w-full px-10 py-20 rounded text-center cursor-pointer border border-dashed border-gray-400 text-gray-400 transition duration-500 hover:text-white hover:bg-green-400 hover:border-green-400 hover:border-solid"
+        :class="{ 'bg-green-400 border-green-400 border-solid': is_dragover }"
+        @drag.prevent.stop=""
+        @dragstart.prevent.stop=""
+        @dragend.prevent.stop="is_dragover = false"
+        @dragover.prevent.stop="is_dragover = true"
+        @dragenter.prevent.stop="is_dragover = true"
+        @dragleave.prevent.stop="is_dragover = false"
+        @drop.prevent.stop="upload($event)"
+      >
+        <h5>Drop your files here</h5>
+      </div>
+      <input
+        type="file"
+        class="mt-3 file:py-1 file:px-1.5 file:rounded file:text-white file:bg-green-600 file:border-0 cursor-pointer file:cursor-pointer"
+        title="Choose a video please"
+        multiple
+        @change="upload($event)"
+      />
+
+      <hr class="my-6" />
+      <!-- Progress Bars -->
+      <div class="mb-4" v-for="upload in uploads" :key="upload.name">
+        <!-- File Name -->
+        <div class="font-bold text-sm" :class="upload.text_class">
+          <i :class="[upload.icon, 'mr-1']"></i>
+          {{ upload.name }}
+        </div>
+        <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
+          <!-- Inner Progress Bar -->
+          <div
+            class="transition-all progress-bar"
+            :class="upload.variant"
+            :style="{ width: upload.current_progress + '%' }"
+          ></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { firebaseApp, songsCollection, storage } from "../includes/firebase";
+export default {
+  name: "Upload",
+  emits: ["add-song", "update-unsaved-flag"],
+  data() {
+    return {
+      is_dragover: false,
+      uploads: [],
+    };
+  },
+  methods: {
+    upload($event) {
+      this.$emit("update-unsaved-flag", true);
+
+      this.is_dragover = false;
+
+      const files = $event.dataTransfer
+        ? [...$event.dataTransfer.files]
+        : [...$event.target.files]; // specify it's from input-upload or drag&drop-upload, then convert it to an Array to loop on it
+
+      files.forEach((file) => {
+        const supportedFormats = ["audio/mpeg", "audio/webm"];
+        if (!supportedFormats.includes(file.type)) {
+          return;
+        }
+
+        // References (objects that point to a location in the app)
+        const storageRef = storage.ref(); // "music-app-374e3.appspot.com", -> bucket where data is stored
+        const songsRef = storageRef.child(`songs/${file.name}`); // music-app-374e3.appspot.com/songs/example.mp3
+        const task = songsRef.put(file); // initialize the uploading process
+
+        const uploadIndex =
+          this.uploads.push({
+            task,
+            current_progress: 0,
+            name: file.name,
+            variant: "bg-blue-400", // progress bar color
+            icon: "fas fa-spinner fa-spin",
+            text_class: "",
+          }) - 1;
+
+        // track upload progress
+        task.on(
+          "state_changed",
+          (snapshot) => {
+            // Snapshot -> object that is a copy of a location in the app (Read-only & Immutable)
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            this.uploads[uploadIndex].current_progress = progress; // update the progress for the progress bar styles
+          },
+          (error) => {
+            // error callback
+            this.$emit("update-unsaved-flag", false);
+            this.uploads[uploadIndex].variant = "bg-red-400";
+            this.uploads[uploadIndex].icon = "fas fa-times";
+            this.uploads[uploadIndex].text_class = "text-red-400";
+            console.log(error);
+          },
+          async () => {
+            console.log(task.snapshot);
+            // success callback
+            const song = {
+              // "snapshot" object hold reference to the current state of the upload
+              uid: firebaseApp.auth().currentUser.uid,
+              display_name: firebaseApp.auth().currentUser.displayName,
+              original_name: task.snapshot.ref.name,
+              modified_name: task.snapshot.ref.name,
+              genre: "",
+              comment_count: 0,
+            };
+
+            song.url = await task.snapshot.ref.getDownloadURL();
+            const songRef = await songsCollection.add(song); // create random ID for the song in the songs collection
+            const songSnapshot = await songRef.get();
+
+            this.$emit("add-song", songSnapshot);
+            this.$emit("update-unsaved-flag", false);
+
+            this.uploads[uploadIndex].variant = "bg-green-400";
+            this.uploads[uploadIndex].icon = "fas fa-check";
+            this.uploads[uploadIndex].text_class = "text-green-400";
+          }
+        );
+      });
+    },
+  },
+  beforeUnmount() {
+    // cancel ongoing tasks when going to another tab/route/component
+    this.uploads.forEach((upload) => {
+      upload.task.cancel();
+    });
+  },
+};
+</script>
--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -3,54 +3,9 @@
   <section class="container mx-auto mt-6">
     <div class="md:grid md:grid-cols-3 md:gap-4">
       <div class="col-span-1">
-        <div
-          class="bg-white rounded border border-gray-200 relative flex flex-col"
-        >
-          <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
-            <span class="card-title">Upload</span>
-            <i class="fas fa-upload float-right text-green-400 text-2xl"></i>
-          </div>
-          <div class="p-6">
-            <!-- Upload Dropbox -->
-            <div
-              class="w-full px-10 py-20 rounded text-center cursor-pointer border border-dashed border-gray-400 text-gray-400 transition duration-500 hover:text-white hover:bg-green-400 hover:border-green-400 hover:border-solid"
-            >
-              <h5>Drop your files here</h5>
-            </div>
-            <hr class="my-6" />
-            <!-- Progess Bars -->
-            <div class="mb-4">
-              <!-- File Name -->
-              <div class="font-bold text-sm">Just another song.mp3</div>
-              <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-                <!-- Inner Progress Bar -->
-                <div
-                  class="transition-all progress-bar bg-blue-400"
-                  style="width: 75%"
-                ></div>
-              </div>
-            </div>
-            <div class="mb-4">
-              <div class="font-bold text-sm">Just another song.mp3</div>
-              <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-                <div
-                  class="transition-all progress-bar bg-blue-400"
-                  style="width: 35%"
-                ></div>
-              </div>
-            </div>
-            <div class="mb-4">
-              <div class="font-bold text-sm">Just another song.mp3</div>
-              <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-                <div
-                  class="transition-all progress-bar bg-blue-400"
-                  style="width: 55%"
-                ></div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <app-upload @addSong="addSong" @updateUnsavedFlag="updateUnsavedFlag" />
       </div>
+
       <div class="col-span-2">
         <div
           class="bg-white rounded border border-gray-200 relative flex flex-col"
@@ -61,115 +16,27 @@
               class="fa fa-compact-disc float-right text-green-400 text-2xl"
             ></i>
           </div>
-          <div class="p-6">
+          <!-- Loading State -->
+          <div class="p-6 flex justify-center" v-if="isLoadingSongs">
+            <i class="fas fa-spinner fa-spin text-5xl text-gray-500"></i>
+          </div>
+          <div class="p-6" v-if="!isLoadingSongs && songs.length > 0">
             <!-- Composition Items -->
-            <div class="border border-gray-200 p-3 mb-4 rounded">
-              <div>
-                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                <button
-                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right"
-                >
-                  <i class="fa fa-times"></i>
-                </button>
-                <button
-                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right"
-                >
-                  <i class="fa fa-pencil-alt"></i>
-                </button>
-              </div>
-              <div>
-                <form>
-                  <div class="mb-3">
-                    <label class="inline-block mb-2">Song Title</label>
-                    <input
-                      type="text"
-                      class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                      placeholder="Enter Song Title"
-                    />
-                  </div>
-                  <div class="mb-3">
-                    <label class="inline-block mb-2">Genre</label>
-                    <input
-                      type="text"
-                      class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                      placeholder="Enter Genre"
-                    />
-                  </div>
-                  <button
-                    type="submit"
-                    class="py-1.5 px-3 rounded text-white bg-green-600"
-                  >
-                    Submit
-                  </button>
-                  <button
-                    type="button"
-                    class="py-1.5 px-3 rounded text-white bg-gray-600"
-                  >
-                    Go Back
-                  </button>
-                </form>
-              </div>
-            </div>
-            <div class="border border-gray-200 p-3 mb-4 rounded">
-              <div>
-                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                <button
-                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right"
-                >
-                  <i class="fa fa-times"></i>
-                </button>
-                <button
-                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right"
-                >
-                  <i class="fa fa-pencil-alt"></i>
-                </button>
-              </div>
-            </div>
-            <div class="border border-gray-200 p-3 mb-4 rounded">
-              <div>
-                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                <button
-                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right"
-                >
-                  <i class="fa fa-times"></i>
-                </button>
-                <button
-                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right"
-                >
-                  <i class="fa fa-pencil-alt"></i>
-                </button>
-              </div>
-            </div>
-            <div class="border border-gray-200 p-3 mb-4 rounded">
-              <div>
-                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                <button
-                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right"
-                >
-                  <i class="fa fa-times"></i>
-                </button>
-                <button
-                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right"
-                >
-                  <i class="fa fa-pencil-alt"></i>
-                </button>
-              </div>
-            </div>
-            <div class="border border-gray-200 p-3 mb-4 rounded">
-              <div>
-                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                <button
-                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right"
-                >
-                  <i class="fa fa-times"></i>
-                </button>
-                <button
-                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right"
-                >
-                  <i class="fa fa-pencil-alt"></i>
-                </button>
-              </div>
-            </div>
+            <composition-item
+              v-for="(song, i) in songs"
+              :key="song.docID"
+              :song="song"
+              :updateSong="updateSong"
+              :index="i"
+              :removeSong="removeSong"
+              @updateUnsavedFlag="updateUnsavedFlag"
+            />
+          </div>
+          <div
+            class="p-6 text-gray-500"
+            v-if="!isLoadingSongs && songs.length === 0"
+          >
+            Currently, You Don't have any songs
           </div>
         </div>
       </div>
@@ -178,7 +45,60 @@
 </template>
 
 <script>
+import CompositionItem from "../components/CompositionItem.vue";
+import AppUpload from "../components/Upload.vue";
+import { firebaseApp, songsCollection } from "../includes/firebase";
 export default {
+  components: { AppUpload, CompositionItem },
   name: "Manage",
+  data() {
+    return {
+      songs: [],
+      unsavedFlag: false,
+      isLoadingSongs: false,
+    };
+  },
+  async created() {
+    // Querying the Database
+    this.isLoadingSongs = true;
+    const snapshot = await songsCollection
+      .where("uid", "==", firebaseApp.auth().currentUser.uid)
+      .get();
+
+    snapshot.forEach(this.addSong);
+    this.isLoadingSongs = false;
+  },
+  methods: {
+    updateSong(songIndex, values) {
+      this.songs[songIndex].modified_name = values.modified_name;
+      this.songs[songIndex].genre = values.genre;
+    },
+    removeSong(i) {
+      this.songs.splice(i, 1);
+    },
+    addSong(songDocument) {
+      const song = {
+        ...songDocument.data(),
+        docID: songDocument.id,
+      };
+
+      this.songs.push(song);
+    },
+    updateUnsavedFlag(value) {
+      console.log(value);
+      this.unsavedFlag = value;
+    },
+  },
+  beforeRouteLeave(to, from, next) {
+    if (!this.unsavedFlag) {
+      next();
+    } else {
+      // eslint-disable-next-line no-alert, no-restricted-globals
+      const leave = confirm(
+        "You have unsaved changes. Are you sure you want to leave?"
+      );
+      next(leave);
+    }
+  },
 };
 </script>
