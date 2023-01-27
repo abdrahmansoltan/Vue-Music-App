34625a0117ab60e15611c3cd864b5a69302f2621
Abdelrahman Soltan <asoltan@instabug.com>
2023-01-27 13:15:28 +0200

    feat: :zap: improve performance for loading song page data

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -132,20 +132,21 @@ export default {
       });
     },
   },
-  async created() {
-    const docSnapshot = await songsCollection.doc(this.$route.params.id).get(); // make request to get the song document
+  async beforeRouteEnter(to, from, next) {
+    const docSnapshot = await songsCollection.doc(to.params.id).get(); // make request to get the song document
+    next((vm) => {
+      // Local Guard Redirection
+      if (!docSnapshot.exists) {
+        vm.$router.push({ name: "home" });
+        return;
+      }
 
-    // Local Guard Redirection
-    if (!docSnapshot.exists) {
-      this.$router.push({ name: "home" });
-      return;
-    }
+      const { sort } = vm.$route.query;
+      vm.sort = sort === "1" || sort === "2" ? sort : "1"; // default sort value = "1"
 
-    const { sort } = this.$route.query;
-    this.sort = sort === "1" || sort === "2" ? sort : "1"; // default sort value = "1"
-
-    this.song = docSnapshot.data();
-    this.getComments();
+      vm.song = docSnapshot.data();
+      vm.getComments();
+    });
   },
   methods: {
     ...mapActions(usePlayerStore, ["newSong"]),
