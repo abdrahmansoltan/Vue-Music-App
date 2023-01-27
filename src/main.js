65d606c2eb267739b8559dd23ab7d8cf45716ed6
Abdelrahman Soltan <asoltan@instabug.com>
2023-01-27 13:44:02 +0200

    config: 🔧 configure progress bar for loading pages using nprogress library

--- src/main.js
+++ src/main.js
@@ -3,17 +3,22 @@ import { registerSW } from "virtual:pwa-register";
 import { createApp } from "vue";
 import { firebaseApp } from "./includes/firebase";
 import i18n from "./includes/i18n";
+import progressBar from "./includes/progress-bar";
 import veeValidatePlugin from "./includes/validation";
 
 import App from "./App.vue";
 import Icon from "./directives/icon";
 import router from "./router";
 
+import "nprogress/nprogress.css";
 import "./assets/base.css";
 import "./assets/main.css";
 
 // Load and Register the PWA Service Worker
 registerSW({ immediate: true });
+
+// align progressBar from nprogress package with the router
+progressBar(router);
 
 let app;
 // initialize the app after firebase use cached data if any to authenticate
