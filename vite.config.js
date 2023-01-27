afd726b7935e368f83b828d594d0ffb6496af5e2
Abdelrahman Soltan <asoltan@instabug.com>
2023-01-27 11:18:56 +0200

    config(PWA): 🔧 configure the manifest file for PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,39 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // method that generates the manifest file & create service worker
+    VitePWA({
+      registerType: "autoUpdate", // automatically update to latest
+      devOptions: {
+        enabled: true, // to prevent creating manifest file in development mode
+      },
+      manifest: {
+        name: "Music App",
+        theme_color: "#ff5e3a",
+        icons: [
+          {
+            src: "assets/img/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        // include file-formats to be cached (e.g. images)
+        globPatterns: ["**/*.{js,css,html,png,jpg,}"],
+      },
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
